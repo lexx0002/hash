e9915e3bd353fd4ca261901c277ed3b009639371
lexx0002 <33981077+lexx0002@users.noreply.github.com>
2019-06-05 10:58:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 
 def generator_mp5(path_to_file):
     with open(path_to_file) as file:
-        file = json.load(file)
         for line in file:
             print(hashlib.md5(str(line).encode('utf-8')).hexdigest())
 

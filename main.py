46b876c36bfd476b7431d6011fac5b99e859bb39
lexx0002 <33981077+lexx0002@users.noreply.github.com>
2019-06-07 08:32:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import hashlib
-import json
 
 def generator_mp5(path_to_file):
     with open(path_to_file) as file:
-        generator = (line for line in file)
-        text = hashlib.md5(str(generator).encode('utf-8')).hexdigest()
+        for line in file:
+            text = hashlib.md5(str(line).encode('utf-8')).hexdigest()
+            yield text
 
-
-        yield text
-
-print(generator_mp5('countries.json').__next__())
-print(generator_mp5('countries.json').__next__())
+for item in generator_mp5('countries.json'):
+    print(item)
